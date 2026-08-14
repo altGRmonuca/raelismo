390c36d0d9746e4266c5439180c8ceec565e6f2f
altGRmonuca <monica.christo@escola.pr.gov.br>
2026-08-14 08:00:45 -0300

    ok..

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
-function toggleSearch() {
+
+        const palavras = ["Elohim", "RAEL", "Yahweh", "Objetivo", "Polêmica"];
+        let intervaloDigitacao;
+
+        function toggleSearch() {
             const searchBox = document.getElementById('searchBox');
+            const searchInput = document.getElementById('searchInput');
+            
+            clearInterval(intervaloDigitacao); 
             searchBox.classList.toggle('active');
+            
             if (searchBox.classList.contains('active')) {
-                searchBox.querySelector('.search-input').focus();
+                searchInput.focus();
+                /
+                const palavraSorteada = palavras[Math.floor(Math.random() * palavras.length)];
+                digitarPalavra(searchInput, palavraSorteada);
+            } else {
+                searchInput.value = ""; 
             }
+        }
+
+        
+        function digitarPalavra(input, texto) {
+            input.value = "";
+            let index = 0;
+            
+            intervaloDigitacao = setInterval(() => {
+                if (index < texto.length) {
+                    input.value += texto.charAt(index);
+                    index++;
+                } else {
+                    clearInterval(intervaloDigitacao);
+                }
+            }, 150); 
         }
